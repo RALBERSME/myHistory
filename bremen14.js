33d6f316b7f8aeacb190a957430b99e2ce8dfe1f
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-07-12 18:09:20 +0200

    upload bremen 14

--- bremen14.js
+++ bremen14.js
@@ -1,19 +1,14 @@
 const rainyContainer = document.getElementById("rainy-container");
 
 function createRaindrop() {
-  // Create a new raindrop element
   const raindrop = document.createElement("div");
   raindrop.classList.add("raindrop");
 
-  // Generate a random number between 0 and the width of the container
   const x = Math.floor(Math.random() * rainyContainer.offsetWidth);
 
-  // Set the position of the raindrop using the generated x value
   raindrop.style.left = `${x}px`;
 
-  // Add the raindrop to the container
   rainyContainer.appendChild(raindrop);
 }
 
-// Create a new raindrop every 55 milliseconds
 setInterval(createRaindrop, 95);
